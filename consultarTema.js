b14203b386068b8d0ccfbb62ff61da41c30f9a26
Luciano Donato <luciano.h.donato@gmail.com>
2022-05-01 15:26:34 -0300

    ajustando criação de tema

--- consultarTema.js
+++ consultarTema.js
@@ -23,43 +23,43 @@ function loadTable() {
 
 loadTable();
 
-function showUserCreateBox() {
-    Swal.fire({
-      title: 'Create user',
-      html:
-        '<input id="id" type="hidden">' +
-        '<input id="fname" class="swal2-input" placeholder="First">' +
-        '<input id="lname" class="swal2-input" placeholder="Last">' +
-        '<input id="username" class="swal2-input" placeholder="Username">' +
-        '<input id="email" class="swal2-input" placeholder="Email">',
-      focusConfirm: false,
-      preConfirm: () => {
-        userCreate();
-      }
-    })
-}
+// function showUserCreateBox() {
+//     Swal.fire({
+//       title: 'Create user',
+//       html:
+//         '<input id="id" type="hidden">' +
+//         '<input id="fname" class="swal2-input" placeholder="First">' +
+//         '<input id="lname" class="swal2-input" placeholder="Last">' +
+//         '<input id="username" class="swal2-input" placeholder="Username">' +
+//         '<input id="email" class="swal2-input" placeholder="Email">',
+//       focusConfirm: false,
+//       preConfirm: () => {
+//         userCreate();
+//       }
+//     })
+// }
 
-function userCreate() {
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
+// function userCreate() {
+//     const fname = document.getElementById("fname").value;
+//     const lname = document.getElementById("lname").value;
+//     const username = document.getElementById("username").value;
+//     const email = document.getElementById("email").value;
       
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify({ 
-      "fname": fname, "lname": lname, "username": username, "email": email, 
-      "avatar": "https://i.pinimg.com/736x/0d/d2/0e/0dd20ebf2a8737a2d9740be6d5877b8b.jpg"
-    }));
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        Swal.fire(objects['message']);
-        loadTable();
-      }
-    };
-}
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
+//     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     xhttp.send(JSON.stringify({ 
+//       "fname": fname, "lname": lname, "username": username, "email": email, 
+//       "avatar": "https://i.pinimg.com/736x/0d/d2/0e/0dd20ebf2a8737a2d9740be6d5877b8b.jpg"
+//     }));
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         const objects = JSON.parse(this.responseText);
+//         Swal.fire(objects['message']);
+//         loadTable();
+//       }
+//     };
+// }
 
 function userDelete(id) {
     const xhttp = new XMLHttpRequest();
