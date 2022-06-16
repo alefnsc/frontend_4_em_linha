c38bf0c93df7e8c1ac1097192c19f9873f13b896
Enrico <enrico.enr07@gmail.com>
2022-06-16 14:31:42 -0300

    alteracoes

--- Inicio/javascript/script.js
+++ Inicio/javascript/script.js
@@ -27,3 +27,25 @@ inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 inputs.forEach((input) => input.addEventListener('input', handleChange));
 
+async function PostLogin() {
+    var nome = document.getElementById('nome').value;
+    console.log(nome)
+    var senha = document.getElementById('senha').value;
+    console.log(senha)
+    await new Promise(r => setTimeout(r, 2000));
+    // const response = await fetch(api_url, {
+    //     method: 'POST',
+    //     headers: {}, //O erro de CORS ocorria quando preenchia o Headers com Access-Control-Allow-Origin e Content-Type-application/json
+    
+    //     //Convertemos antes os valores de lat e lng pois estavam vindo como string, mas no console pareciam ser inteiros, para não dar erro de tipo
+    //     body: JSON.stringify(
+    //       {
+    //         nome: nome,
+    //         senha: senha
+    //       }
+    //     )
+    //   })
+    window.location.href = "saguao.html";
+}
+
+
