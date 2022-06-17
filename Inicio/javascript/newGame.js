2ed1b811ef0e323e2e2efcce716a4856cd823731
alefnsc <alexandrefonsecach@gmail.com>
2022-06-16 21:36:59 -0300

    setCampos listen

--- Inicio/javascript/newGame.js
+++ Inicio/javascript/newGame.js
@@ -6,11 +6,41 @@ const tabJog = document.getElementById('vezJogador')
 const largura = 7
 const altura = 6
 const tabuleiro = altura * largura
+var campos = [];
+
+
+for (let i = 0; i < altura * largura; i++) {
+    campos[i] = 0
+}
+//setInterval(mostraTabela , 1000)
+
+const connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/jogo").build();
+start();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+        mostraTabela(1, 1)
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+
+async function setCampos(campos, ultimoDaAltura, player)
+{
+    await connection.send("setCampos", campos, ultimoDaAltura, player == 1 ? 2 : 1);
+ }
+
+        connection.on("setCampos", (campos, ultimoDaAltura, player) => {
+        console.log(campos);
+        mostraTabela(ultimoDaAltura, player);
+   });
+
 
 function marcar(player, X) {
-
-    // trazer array campos do back
-
     if (X > largura || player == 0) {
         return false
     }
@@ -22,13 +52,11 @@ function marcar(player, X) {
         if (campos[ultimoDaAltura] == 0) {
             campos[ultimoDaAltura] = player == 1 ? 1 : 2;
             var Y = altura - (i + 1);
-            setCampos(campos, player, X, Y);
-            mostraTabela(ultimoDaAltura, player == 1 ? 2 : 1);
+            setCampos(campos, ultimoDaAltura, player);
             return false
         }
     }
 }
-
 
 function mostraTabela(atual, player) {
    // Vez Jogador
@@ -46,7 +74,6 @@ function mostraTabela(atual, player) {
    tabHelper.innerHTML = helper
 
    //trazer array campos back
-   campos = getCampos();
 
    var html = "<table class='tabelaPrincipal'>"
    for (let i = 0; i < tabuleiro / largura; i++) {
